798114cea64ca87f8ce6ab046da1800e0d8561ec
plungerman <plungerman@gmail.com>
2016-06-14 09:11:05 -0500

    added department, funding, approved, fields; new Funding() data model class that is a foreignkey 1toMany relationship to Proposal(), where Proposal can have many Funding() relationships

--- djbeca/core/models.py
+++ djbeca/core/models.py
@@ -8,6 +8,7 @@ class Proposal(models.Model):
     """
     Proposal to pursue funding
     """
+
     user = models.ForeignKey(User)
     updated_by = models.ForeignKey(
         User,
@@ -21,16 +22,26 @@ class Proposal(models.Model):
     updated_at = models.DateTimeField(
         "Date Updated", auto_now=True
     )
+    department = models.CharField(
+        max_length=12
+    )
     title = models.CharField(
         "Proposal title", max_length=255
     )
     summary = models.TextField(
         "Proposal summary"
     )
+    funding = models.BooleanField(
+        "Do you have a funding source?",
+        help_text="Check the box if you have found a funding source",
+        default=False
+    )
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering  = ['-created_at']
         get_latest_by = 'created_at'
+        db_table = 'core_proposal'
 
     def __unicode__(self):
         """
@@ -41,3 +52,13 @@ class Proposal(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('proposal_detail', [str(self.id)])
+
+
+'''
+class Funding(models.Model):
+    """
+    Funding source(s) for proposal
+    """
+    proposal = ForeignKey(Proposal, null=True, blank=True)
+'''
+
