f2c5194ac9c8e3775643a11819b230f34c04abea
plungerman <plungerman@gmail.com>
2016-06-14 09:08:55 -0500

    dynamically set the department choices based on the user's membership in said departments; exclude 'approved' field

--- djbeca/core/forms.py
+++ djbeca/core/forms.py
@@ -6,7 +6,20 @@ from djbeca.core.models import Proposal
 
 class ProposalForm(forms.ModelForm):
 
+    def __init__(self, department_choices, *args, **kwargs):
+        super(ProposalForm, self).__init__(*args, **kwargs)
+        choices = ()
+        if department_choices:
+            choices = department_choices
+        #self.fields['department'].choices = choices
+        self.fields['department'].choices = department_choices
+
+    department = forms.ChoiceField(
+        label="Department",
+        choices=()
+    )
+
     class Meta:
         model = Proposal
         #fields = '__all__'
-        exclude = ('user',)
+        exclude = ('user','approved')
