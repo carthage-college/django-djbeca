81eb96339c363a15e98b56626301345210f77418
plungerman <plungerman@gmail.com>
2016-06-14 09:14:17 -0500

    mostly email infrastructure and departments logic

--- djbeca/core/views.py
+++ djbeca/core/views.py
@@ -6,6 +6,8 @@ from django.core.urlresolvers import reverse_lazy
 
 from djbeca.core.forms import ProposalForm
 
+from djzbar.utils.hr import person_departments
+from djzbar.utils.hr import department_divison_chairs
 from djzbar.decorators.auth import portal_auth_required
 from djtools.utils.mail import send_mail
 
@@ -19,29 +21,40 @@ logger = logging.getLogger(__name__)
 )
 def index(request):
     if settings.DEBUG:
-        TO_LIST = [settings.SERVER_EMAIL,]
+        TO_LIST = [settings.ADMINS[0][1],]
     else:
         TO_LIST = [settings.PROPOSAL_EMAIL,]
     BCC = settings.MANAGERS
 
+    depts = person_departments(request.user.id)
     if request.method=='POST':
-        form = ProposalForm(request.POST, request.FILES)
+        form = ProposalForm(depts, request.POST)
         if form.is_valid():
-            data = form.save()
-            email = settings.DEFAULT_FROM_EMAIL
-            if data.email:
-                email = data.email
-            subject = "[Proposal] {} {}".format(
-                data.last_name,data.first_name
+            data = form.save(commit=False)
+            data.user = request.user
+            data.save()
+            subject = "[OSP Proposal] {}: {}, {}".format(
+                data.title, data.user.last_name, data.user.first_name
             )
+            chairs = department_divison_chairs(data.department)
+            if not settings.DEBUG:
+                # Department chair's email
+                TO_LIST.append(chairs[0][4])
+                # Division chair's email
+                TO_LIST.append(chairs[0][8])
+            else:
+                data.chairs = chairs
+            # temporarily assign department to full name
+            data.department = chairs[0][0]
             send_mail(
-                request,TO_LIST, subject, email,"proposal/email.html", data, BCC
+                request, TO_LIST, subject, data.user.email,
+                "proposal/email.html", data, BCC
             )
             return HttpResponseRedirect(
                 reverse_lazy("proposal_success")
             )
     else:
-        form = ProposalForm()
+        form = ProposalForm(depts)
     return render_to_response(
         "proposal/form.html",
         {"form": form,},
