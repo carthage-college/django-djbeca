377f55883247c291d599b56857ab94b6fc9b0e49
plungerman <plungerman@gmail.com>
2017-08-31 14:09:34 -0500

    added logic to change email distribution list depending on debug settings

--- djbeca/core/signals.py
+++ djbeca/core/signals.py
@@ -9,15 +9,22 @@ from djtools.utils.mail import send_mail
 @receiver(post_save, sender=ProposalImpact)
 def proposal_impact_poste_save(sender, **kwargs):
     obj = kwargs['instance'].proposal
-    if not obj.decline and obj.step1() and obj.step2() and not obj.email_approved:
+    if not obj.decline and obj.step1() and obj.step2() \
+      and not obj.email_approved:
+
+        if settings.DEBUG:
+            to_list = [settings.SERVER_EMAIL,]
+        else:
+            to_list = [settings.PROPOSAL_EMAIL,]
+
         # send the email
         subject = "Proposal approved {}: {}, {}".format(
             obj.title, obj.user.last_name, obj.user.first_name
         )
         sent = send_mail(
-            kwargs.get("request"), [settings.PROPOSAL_EMAIL,],
+            kwargs.get('request'), to_list,
             subject, settings.SERVER_MAIL,
-            "proposal/email.html", obj, settings.MANAGERS
+            'proposal/email.html', obj, settings.MANAGERS
         )
         if sent:
             obj.email_approved = True
