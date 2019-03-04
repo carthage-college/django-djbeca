cca0a7395d4e27c7fbdc25e6c1ea6c03a30ac244
plungerman <plungerman@gmail.com>
2019-03-04 13:14:19 -0600

    allow pdf and xls/xlsx file types for upload

--- djbeca/core/models.py
+++ djbeca/core/models.py
@@ -4,15 +4,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
+from django.core.validators import FileExtensionValidator
 
 from djbeca.core.choices import *
 
 from djtools.utils.users import in_group
 from djtools.fields.helpers import upload_to_path
-from djtools.fields.validators import MimetypeValidator
 from djzbar.utils.hr import chair_departments, get_position
 
 from taggit.managers import TaggableManager
+
+ALLOWED_EXTENSIONS = [
+    'xls','xlsx','pdf'
+]
+
+FILE_VALIDATORS = [
+    FileExtensionValidator(allowed_extensions=ALLOWED_EXTENSIONS)
+]
 
 
 class Proposal(models.Model):
@@ -657,16 +665,16 @@ class ProposalBudget(models.Model):
     budget_final = models.FileField(
         "Final Budget for Review",
         upload_to=upload_to_path,
-        #validators=[MimetypeValidator('application/pdf')],
+        validators=FILE_VALIDATORS,
         max_length=768,
-        help_text="PDF format Only"
+        help_text="PDF or Excel Format Only"
     )
     budget_justification_final = models.FileField(
         "Final Budget Justification for Review",
         upload_to=upload_to_path,
-        #validators=[MimetypeValidator('application/pdf')],
+        validators=FILE_VALIDATORS,
         max_length=768,
-        help_text="PDF format Only",
+        help_text="PDF or Excel Format Only"
         null=True,blank=True
     )
 
@@ -702,9 +710,9 @@ class ProposalDocument(models.Model):
     phile = models.FileField(
         "Supporting Document",
         upload_to=upload_to_path,
-        #validators=[MimetypeValidator('application/pdf')],
+        validators=FILE_VALIDATORS,
         max_length=768,
-        help_text="PDF Format Only",
+        help_text="PDF or Excel Format Only"
         null=True,blank=True
     )
     tags = TaggableManager(blank=True)
