d8d923fb2a1beb5f68a833636b6a93c25097d582
plungerman <plungerman@gmail.com>
2022-12-12 13:46:31 -0600

    removed old code

--- djbeca/static/js/main.js
+++ djbeca/static/js/main.js
@@ -1,35 +1,3 @@
-// Opera 8.0+
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-// Safari 3.0+ "[object HTMLElementConstructor]"
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-// Edge 20+
-var isEdge = !isIE && !!window.StyleMedia;
-// Chrome 1 - 71
-var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-// Blink engine detection
-var isBlink = (isChrome || isOpera) && !!window.CSS;
-
-if (isFirefox==false && isChrome==false && isOpera==false && isSafari==false) {
-    alert('Please use the FireFox, Chrome, Safari 12+, or Opera Browser');
-}
-
-/*
-var output = 'Detecting browsers by ducktyping:<hr>';
-output += 'isFirefox: ' + isFirefox + '<br>';
-output += 'isChrome: ' + isChrome + '<br>';
-output += 'isSafari: ' + isSafari + '<br>';
-output += 'isOpera: ' + isOpera + '<br>';
-output += 'isIE: ' + isIE + '<br>';
-output += 'isEdge: ' + isEdge + '<br>';
-output += 'isBlink: ' + isBlink + '<br>';
-//document.body.innerHTML = output;
-console.log(output);
-*/
-
 /* spinner: instantiate */
 var opts = {
   lines: 13, // The number of lines to draw
